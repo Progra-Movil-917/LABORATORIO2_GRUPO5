34efbc07154b45704e260524e4c6f76bd9e2fe2f
Iris Vanessa Sagastume Nataren <167151929+Iris-Sagastume@users.noreply.github.com>
2024-05-08 20:44:47 -0600

    ejemplo stateful

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_laboratorio2_grupo5
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_laboratorio2_grupo5/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_laboratorio2_grupo5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
